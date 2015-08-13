997d01291288186c1b8405929ef2704cb4f7c8f2
Valiev <airat.valiev@gmail.com>
2015-08-13 23:31:50 +0300

    boostrap.groovy clean

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,56 +1,17 @@
-import org.apache.log4j.Logger
-import ru.avtopark.City
-
-import ru.avtopark.Route
 import ru.avtopark.Settings
 
 class BootStrap {
 
     def init = { servletContext ->
 
-//       if (City.count<1) {
-//           new City(name: "Москва",urlName: "moskva",desc: "Описание").save()
-//           new City(name: "Санкт-Петербург",urlName: "peterburg",desc: "Описание").save()
-//           new City(name: "Казань",urlName: "kazan",desc: "Описание").save()
-//           new City(name: "Самара",urlName: "samara",desc: "Описание").save()
-//           new City(name: "Саратов",urlName: "saratov",desc: "Описание").save()
-//           new City(name: "Волгоград",urlName: "volgograd",desc: "Описание").save()
-//           new City(name: "Нижний Новгород",urlName: "nizhnij_novgorod",desc: "Описание").save()
-//           new City(name: "Набережные Челны",urlName: "naberezhnye_chelny",desc: "Описание").save()
-//           new City(name: "Нижнекамск",urlName: "nizhnekamsk",desc: "Описание").save()
-//           new City(name: "Астрахань",urlName: "astrahan",desc: "Описание").save()
-//           new City(name: "Пермь",urlName: "perm",desc: "Описание").save()
-//           new City(name: "Екатеринбург",urlName: "ekaterinburg",desc: "Описание").save()
-//           new City(name: "Уфа",urlName: "ufa",desc: "Описание").save()
-//           new City(name: "Тюмень",urlName: "tyumen",desc: "tyumen").save()
-//       }
-
-//       if (Route.count<1) {
-//            def cities=City.list();
-//
-//           for (City cityFrom: cities) {
-//               for (City cityTo: cities) {
-//                   if (cityFrom.urlName!=cityTo.urlName) {
-//                       Route route=new Route();
-//                       route.setUrlName(cityFrom.urlName+'-'+cityTo.urlName);
-//                       route.setName(cityFrom.name+' - '+cityTo.name)
-//                       route.setDepartureCity(cityFrom);
-//                       route.setDestinationCity(cityTo);
-//                       route.save();
-//                   }
-//               }
-//           }
-//       }
-
-
         if (Settings.count<1) {
             new Settings(param_key: "address", param_value: "г. Казань, ул. Оренбургский тракт, д.20").save();
-            new Settings(param_key: "phone", param_value: "8-800-700-6798").save();
+            new Settings(param_key: "phone", param_value: "8 800 500-67-94").save();
             new Settings(param_key: "email", param_value: "logist.d1@mail.ru").save();
-            new Settings(param_key: "fax", param_value: "8-843-277-90-15").save();
-            new Settings(param_key: "tel_kazan", param_value: "8(843) 277-90-15").save();
-            new Settings(param_key: "tel_spb", param_value: "8-800-700-6798").save();
-            new Settings(param_key: "tel_moscow", param_value: "8-800-700-6798").save();
+            new Settings(param_key: "fax", param_value: "8(843) 212-23-84").save();
+            new Settings(param_key: "tel_kazan", param_value: "8(843) 212-23-84").save();
+            new Settings(param_key: "tel_spb", param_value: "(812) 385-57-94").save();
+            new Settings(param_key: "tel_moscow", param_value: "(499) 322-17-94").save();
             new Settings(param_key: "about", param_value: "Грузоперевозки по России любым видом транспорта!").save();
         }
     }
